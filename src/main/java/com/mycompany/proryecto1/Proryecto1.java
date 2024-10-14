676959f9147de52f0215adddbaf18d39673c0e49
MonchoKarbo <146631954+MonchoKarbo@users.noreply.github.com>
2024-10-14 17:21:22 +0200

    Primer commit

---
+++ src/main/java/com/mycompany/proryecto1/Proryecto1.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.proryecto1;
+
+/**
+ *
+ * @author Moncho
+ */
+public class Proryecto1 {
+
+    public static void main(String[] args) {
+        int numero=5;
+        
+        System.out.println("El factorial de "+numero+" es "+factorial(numero));
+        
+    }
+    
+    private static int factorial(int numero){
+        if(numero>1){
+            return numero*factorial(numero-1);
+        }else{
+            return 1;
+        }
+    
+    }
+
+}
