0ea430d61d5e523d658ef9ae8953cd300bde2f27
MonchoKarbo <146631954+MonchoKarbo@users.noreply.github.com>
2024-10-14 17:34:12 +0200

    Primer commit

---
+++ src/main/java/com/mycompany/proryecto1/Circulo.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proryecto1;
+
+/**
+ *
+ * @author Moncho
+ */
+public class Circulo {
+    private double radio;
+
+    public Circulo(double radio) {
+        this.radio = radio;
+    }
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+    
+    
+    
+}
